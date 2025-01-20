fba807f222887736e261f22ef1e5f69c9c7a9a98
Milind Kopikar <milindkopi@gmail.com>
2025-01-19 21:57:57 -0500

    Initial commit

---
+++ supernote.py
@@ -0,0 +1,170 @@
+import os
+from dotenv import load_dotenv
+import openai
+import requests
+import json
+import time
+from datetime import datetime
+import streamlit as st
+
+import utils
+
+
+#Application to load the OpenAI and other keys. 
+load_dotenv()
+
+#Get the OpenAI Model parameters
+#Set the OpenAI API key from Streamlit Secrets Key
+#openai.api_key = st.secrets["OPENAI_API_KEY"]
+client = openai.OpenAI()
+model = utils.get_openai_model() #Get the OpenAI model (e.g: GPT 4o mini)
+
+# Initialize all the sessions in Steamlit. States are used so that code is not called each time the page is refreshed.
+if "file_id_list" not in st.session_state:
+    st.session_state.file_id_list = [] #List of file ids uploaded by the user
+
+if "start_chat" not in st.session_state:
+    st.session_state.start_chat = False #Set the chat session to False. This will be set to True when the user starts chatting.
+
+if "thread_id" not in st.session_state:
+    st.session_state.thread_id = None #Set the thread_id to None. This will be set to the thread_id when the user starts chatting.
+
+if "assistant_id" not in st.session_state:
+    st.session_state.assistant_id = utils.get_assistant_id() #Get the OpenAI Assistant ID
+
+if "vector_store_id" not in st.session_state:
+    st.session_state.vector_store_id = utils.get_vector_store_id() #Get the OpenAI Vector Store ID
+
+if "openai_model" not in st.session_state:
+    st.session_state.openai_model = model #Set the OpenAI model to the model that we have initialized.
+
+# Set up our front end page
+st.set_page_config(page_title="FBLA Interactive Story Teller", page_icon=":books:")
+
+# ==== Function definitions etc ===== #
+# Function to upload the file to OpenAI
+# This function will upload the file to OpenAI and return the file ID
+# This file ID will be used to create the story
+# input: filepath - the path of the file to be uploaded, client - the OpenAI client
+# output: response.id - the file ID of the uploaded file   
+def upload_to_openai(filepath, client):
+    file_upload_status = utils.upload_file_to_vector_store(client, st.session_state.vector_store_id, filepath)
+    return file_upload_status
+
+# === Sidebar - where users can upload files
+file_uploaded = st.sidebar.file_uploader(
+    "Upload the notes from your science class.", 
+    key="file_upload"
+)
+
+# Upload file button - store the file ID
+if st.sidebar.button("Upload File"):
+    if file_uploaded:
+        with open(f"{file_uploaded.name}", "wb") as f:
+            f.write(file_uploaded.getbuffer())
+        st.sidebar.write(f"Notes uploaded: {file_uploaded.name}")
+        #Upload the file to OpenAI and get the file ID 
+        file_upload_status = upload_to_openai(f"{file_uploaded.name}", client)
+        #Append the file ID to the session state.
+        st.session_state.file_id_list.append(file_upload_status)
+        # st.sidebar.write(f"file_upload_status:: {file_upload_status}")
+
+# Display those file ids
+if st.session_state.file_id_list:
+    st.sidebar.write("Uploaded File IDs:")
+    #Check if these files exist in the flie list.
+    file_objects = client.files.list()
+    if file_objects == None:
+        st.sidebar.warning("No files found in Open AI.")
+    else:
+        for file_id in st.session_state.file_id_list:
+            for file in file_objects:
+                if file.id == file_id:
+                    st.sidebar.write(f"File.id in Client: {file.id, file.filename}")
+    # #Print the files in the st.session_state.file_id_list. 
+    # # These should be the ones that are also attached to the OpenAI Client
+    # for file_id in st.session_state.file_id_list:
+    #      st.sidebar.write(file_id)
+
+#Set the streamlit session for chatting and add create the Open AI Thread. 
+# Create a new thread for this chat session if one does not exist already.
+# If it exists, then use the existing thread. 
+if not st.session_state.thread_id: 
+    chat_thread = client.beta.threads.create()
+    st.session_state.thread_id = chat_thread.id
+
+#Set the Streamlit state for Chat Session to True to indicate that the chat has started.
+st.session_state.start_chat = True
+
+# The main interface ...
+st.title("SuperNote Taker")
+st.write("Get the complete notes on what your teacher has taught in class. This is crowdsourced from all the student's notes.")
+st.write("You can get a summary of the notes, ask for explanations on topics covered in the notes, get flashcards or quizlets or event translate the notes into your preferred language")
+
+# Check sessions
+if st.session_state.start_chat:
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    # Show existing messages if any. 
+    # Note that the streamlit runs this file each time. So this line will display all the messages so far.
+    # It will display the message  
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    # chat input for the user
+    prompt = st.chat_input("Type...")
+    if prompt:
+        # Add user message to the state and display on the screen
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        # add the user's message to the existing Open AI Assistant's Thread of Messages. 
+        client.beta.threads.messages.create(
+            thread_id=st.session_state.thread_id, role="user", content=prompt
+        )
+
+        # Create a run with additioal instructions
+        run = client.beta.threads.runs.create(
+            thread_id=st.session_state.thread_id,
+            assistant_id=st.session_state.assistant_id  
+        )
+
+        # Show a spinner while the assistant is thinking...
+        with st.spinner("Wait... Generating response..."):
+            while run.status != "completed":
+                time.sleep(1)
+                run = client.beta.threads.runs.retrieve(
+                    thread_id=st.session_state.thread_id, run_id=run.id
+                )
+            # Retrieve messages added by the openAI Assistant to the current Thread
+            messages = client.beta.threads.messages.list(
+                thread_id=st.session_state.thread_id
+            )
+            # Go through all the messages and append the assistant message to assistant_messages
+            assistant_messages_for_run = []
+            for message in messages:
+                if message.run_id == run.id and message.role == "assistant":
+                    assistant_messages_for_run.append(message)
+
+            # assistant_messages_for_run = [
+            #     message
+            #     for message in messages
+            #     if message.run_id == run.id and message.role == "assistant"
+            # ]
+
+            for message in assistant_messages_for_run:
+                # full_response = process_message_with_citations(message=message)
+                full_response = message.content[0].text.value
+                st.session_state.messages.append(
+                    {"role": "assistant", "content": full_response}
+                )
+                with st.chat_message("assistant"):
+                    st.markdown(full_response, unsafe_allow_html=True)
+
+
+
+
+
---
+++ utils.py
@@ -0,0 +1,122 @@
+#This file contains all the functions that are needed for the FBLA Streamlit code to run. 
+import os
+from dotenv import load_dotenv
+import openai
+import logging
+
+#Application to load the OpenAI and other keys. 
+load_dotenv()
+
+#Function to return the Open AI Client. 
+def get_openai_client():
+    client = openai.OpenAI()
+    return client
+
+#Function to get the Open AI Model
+def get_openai_model():
+    return "gpt-4o-mini"
+
+#Function to return the Open AI Assistant ID
+def get_assistant_id():
+    return "asst_A3XOjQiGb0wxlWnvXH1BDLy4"
+
+#Function to return the Vector Store ID
+def get_vector_store_id():
+    return "vs_iZ5WSjRU4Mq2izbJYIrrCgWb"
+
+#Write a function that checks if the Assitant with the given name already exists.
+#If it exists, simply return the ID for that Assistant so we can use it in our code. 
+#But if it does not exist, Create that Assistant and return Assistant 
+#This way, we will avoid having to create a whole new Assistant again. 
+# Input: Name of Assistant, OpenAI client, OpenAI model to use
+def check_and_get_assistant(assistant_name, client):
+    #Get a list of existing assistants in descenidng order of dates. 
+    my_assistants = client.beta.assistants.list(
+        order="desc",
+        limit="20",
+    )
+    for assistant in my_assistants:
+        if assistant.name == assistant_name:
+            return assistant
+    #Assistant not found. Create one. 
+    # If it does exist, update the instructions for that Assistant with the latest instructions above.  
+    assistant = client.beta.assistants.create(
+            name=assistant_name,
+            model=get_openai_model(),
+            tools=[{"type": "file_search"}], 
+    )
+    return assistant
+
+# Write a function that updates the  instructions for that Assistant
+# Input: OpenAI client, Assistant ID, Instructions for Assistant, 
+# Output: Assistant
+def updated_assistant_instruction(client, assistant_id, assiatant_instruction):
+    #Update the instructions for this assistant. 
+    assistant = client.beta.assistants.update(
+        assistant_id,
+        instructions= assiatant_instruction
+    )
+    return assistant
+
+# #Check if the vectore store by this name exists. 
+# And if not, create such a vectore store. 
+# Input: Open AI Client, Vector Store Name
+# Output: Vector Store
+def check_and_get_vector_store(client, vs_name):
+    vs_list = client.beta.vector_stores.list()
+    for vs in vs_list:
+        if vs.name == vs_name:
+            return vs
+    # # Vector Store does not exist. Create one. 
+    vs = client.beta.vector_stores.create(vs_name)
+    return vs
+
+# # Function to check if a file exists in the vector store.
+# Input: Open AI Client, Vectore Store ID, Filename
+# Output: File object in vector store (if file exists); False if file does not exist in vectore store
+def file_exists_in_vector_store(client, vector_store_id, filename):
+    # List all files in the vector store
+    response = client.beta.vector_stores.files.list(vector_store_id=vector_store_id)
+    files = response.data
+    
+    # Check if any file has the same name as the target filename
+    for file in files:
+        # Retrieve file details to get the filename
+        file_details = client.files.retrieve(file_id=file.id)
+        if file_details.filename == filename:
+            return file_details
+    # File does not exist. Return False. 
+    return False 
+
+#Upload File.
+# Input: Open AI Client, Vectore Store ID, Filename
+#Output: Return Vector Sore ID
+def upload_file_to_vector_store(client, vector_store_id, filename):
+    ##Check if the file with that filename exists
+    file_object = file_exists_in_vector_store(client, vector_store_id, filename)
+    if not file_object:
+        #File does not exist. Upload file and attach to VS
+        # Ready the files for upload to OpenAI
+        #Add all the files in an array called file_paths
+        file_paths = [filename]
+        #Append these files to file_streams
+        file_streams = [open(path, "rb") for path in file_paths]
+        # # Use the upload and poll SDK helper to upload the files, add them to the vector store,
+        # # and poll the status of the file batch for completion.
+        file_batch = client.beta.vector_stores.file_batches.upload_and_poll(
+            vector_store_id=vector_store_id, files=file_streams
+        )
+        #Get the File that was uploaded. 
+        file_object = file_exists_in_vector_store(client, vector_store_id, filename)
+    #Return the File ID of the file that was uploaded to the Vectore Store. 
+    return file_object
+
+# # Update the assistant to use the updated Vector Store
+# Input: Open AI Client, Assistant_ID, Vector_Store_ID
+# Output: Assistant
+def update_assistant_vector_store(client, assistant_id, vs_id):
+    assistant = client.beta.assistants.update(
+        assistant_id=assistant_id,
+        tool_resources={"file_search": {"vector_store_ids": [vs_id]}},
+    )
+    return assistant
